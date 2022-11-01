2cc64df0ea019195439b8648a62f83b09ef674af
gidyon <gideon.ngk@gmail.com>
2022-11-01 08:02:52 +0300

    redis cache

---
+++ cache/redis/doc.go
@@ -0,0 +1,4 @@
+/*
+Package redis implements USSD caching using redis client.
+*/
+package redis
--- cache/redis/redis.go
+++ cache/redis/redis.go
@@ -2,36 +2,17 @@ package redis
 
 import (
 	"context"
+	"errors"
 	"time"
 
 	"github.com/gidyon/ussdapp"
 	"github.com/go-redis/redis/v8"
 )
 
-func NewRedisCache(opt *Options, expireDur time.Duration) ussdapp.Cacher {
-	redisClient := redis.NewClient(&redis.Options{
-		Network:            "tcp",
-		Addr:               opt.Addr,
-		Username:           opt.Username,
-		Password:           opt.Password,
-		DB:                 opt.DB,
-		MaxRetries:         opt.MaxRetries,
-		MinRetryBackoff:    opt.MinRetryBackoff,
-		MaxRetryBackoff:    opt.MaxRetryBackoff,
-		DialTimeout:        opt.DialTimeout,
-		ReadTimeout:        opt.ReadTimeout,
-		WriteTimeout:       opt.WriteTimeout,
-		PoolFIFO:           opt.PoolFIFO,
-		PoolSize:           opt.PoolSize,
-		MinIdleConns:       opt.MinIdleConns,
-		MaxConnAge:         opt.MaxConnAge,
-		PoolTimeout:        opt.PoolTimeout,
-		IdleTimeout:        opt.IdleTimeout,
-		IdleCheckFrequency: opt.IdleCheckFrequency,
-	})
-
+// NewRedisCache creates a USSD cacher using redis
+func NewRedisCache(opt *redis.Options) ussdapp.Cacher {
+	redisClient := redis.NewClient(opt)
 	rc := &redisCache{cc: redisClient}
-
 	return rc
 }
 
@@ -40,15 +21,23 @@ type redisCache struct {
 	dur time.Duration
 }
 
-func (rc *redisCache) Set(ctx context.Context, key, value string) error {
-	return rc.cc.Set(ctx, key, value, rc.dur).Err()
+func (rc *redisCache) Set(ctx context.Context, key, value string, dur time.Duration) error {
+	return rc.cc.Set(ctx, key, value, dur).Err()
 }
 
 func (rc *redisCache) Get(ctx context.Context, key string) (string, error) {
-	return rc.cc.Get(ctx, key).Result()
+	res, err := rc.cc.Get(ctx, key).Result()
+	switch {
+	case err == nil:
+		return res, nil
+	case errors.Is(err, redis.Nil):
+		return "", ussdapp.ErrKeyNotFound
+	default:
+		return "", err
+	}
 }
 
-func (rc *redisCache) Delete(ctx context.Context, key, value string) error {
+func (rc *redisCache) Delete(ctx context.Context, key string) error {
 	return rc.cc.Del(ctx, key).Err()
 }
 
@@ -74,4 +63,16 @@ func (rc *redisCache) GetMapField(ctx context.Context, key, field string) (strin
 
 func (rc *redisCache) DeleteMapField(ctx context.Context, key string, fields ...string) error {
 	return rc.cc.HDel(ctx, key, fields...).Err()
+}
+
+func (rc *redisCache) SetUnique(ctx context.Context, key string, value string) (bool, error) {
+	res, err := rc.cc.SAdd(ctx, key, value).Result()
+	if err != nil {
+		return false, err
+	}
+	return res == 1, nil
+}
+
+func (rc *redisCache) ExistInSet(ctx context.Context, key, value string) (bool, error) {
+	return rc.cc.SIsMember(ctx, key, value).Result()
 }
